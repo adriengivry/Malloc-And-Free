ca9f46a22c4b0e3b5f99e3464b60be3ff397e0c6
Adrien GIVRY <adrien@givry.pro>
2018-01-29 15:16:54 -0500

    Oups, I forget to add a description to my commit

--- .smartgit/push.sh
+++ .smartgit/push.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ -z "$1" ]
     then
         MESSAGE="Oups, I forget to add a description to my commit"
@@ -6,17 +8,27 @@ if [ -z "$1" ]
 
 fi
 
-if [ -z "$2" ]
-    then
-        REMOTE="origin"
-        echo "Selected remote : origin (By default)"
-    else
-        REMOTE=$2
-        echo "Selected remote : $REMOTE"
-
-fi
-
 git add .
 git commit -m "$MESSAGE"
-git push $REMOTE master
-git push $REMOTE master
+
+if [ "$2" == "*" ]; 
+    then
+        echo "Okey, let's push on every remotes"
+        while IFS='' read -r LINE || [[ -n "$LINE" ]]; 
+            do
+                git push $LINE master
+                echo "Pushed on remote : $LINE"
+        done < ".smartgit/remote.list"
+        
+    else
+        if [ -z "$2" ]
+            then
+                REMOTE="origin"
+                echo "Selected remote : origin (By default)"
+            else
+                REMOTE=$2
+                echo "Selected remote : $REMOTE"
+
+        fi
+        git push $REMOTE master
+fi
