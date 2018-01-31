9ea70dabd7b413ffad43c499d1606b7cd28dfc5f
Adrien GIVRY <a.givry@student.isartdigital.com>
2018-01-31 11:21:22 -0500

    Refactor malloc_perso.h

--- include/malloc_perso.h
+++ include/malloc_perso.h
@@ -12,16 +12,15 @@
 typedef struct s_block t_block;
 struct s_block
 {
-    bool to_free;       // 1 byte
-    size_t size;        // 8 bytes
-    void* data;         // 8 bytes
-    t_block* next;      // 8 bytes
-    t_block* previous;  // 8 bytes
-    
+    bool        to_free;       // 1 byte
+    size_t      size;        // 8 bytes
+    void*       data;         // 8 bytes
+    t_block*    next;      // 8 bytes
+    t_block*    previous;  // 8 bytes
 };
 
-void initialize_block(t_block* block);
-t_block* extend_heap(size_t size);
-void* malloc_perso(size_t size);
+void        initialize_block(t_block* block);
+t_block*    extend_heap(size_t size);
+void*       malloc_perso(size_t size);
 
 #endif // __MALLOC_PERSO__
