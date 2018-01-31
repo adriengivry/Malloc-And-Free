22011ca13a7f299e90cb2a4bd0ea3a8778b55107
Adrien GIVRY <a.givry@student.isartdigital.com>
2018-01-31 15:27:43 -0500

    Refactor memory_block.h

--- include/memory_perso/memory_block.h
+++ include/memory_perso/memory_block.h
@@ -9,11 +9,11 @@ typedef struct s_block t_block;
 
 struct s_block
 {
-    bool        to_free;       // 1 byte
-    size_t      size;        // 8 bytes
-    void*       data;         // 8 bytes
-    t_block*    next;      // 8 bytes
-    t_block*    previous;  // 8 bytes
+    bool        to_free;        // 1 byte
+    size_t      size;           // 8 bytes
+    void*       data;           // 8 bytes
+    t_block*    next;           // 8 bytes
+    t_block*    previous;       // 8 bytes
 };
 
 void iterate(t_block** block);
